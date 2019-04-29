ba093789562c10565e1aa11fc1ec19e7b127503c
Szymon Rozek <rozek1szymon@gmail.com>
2019-04-29 14:55:55 +0200

    ScriptsToSql updated

---
+++ AcceptanceChecker/AcceptanceChecker/Models/DataWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task1PackagesSender.Models
+{
+    public class DataWrapper
+    {
+        public FormData formData { get; set; }
+        public Employees currentUser { get; set; }
+       
+    
+    }
+}
